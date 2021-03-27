5b864f52767449460b4858b303777be99fd8f169
Craiqser <craiqser@gmail.com>
2021-03-27 11:39:00 +0300

    Calculate max elemets of graph (lesson 22).

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ export default {
 		return {
 			filter: '',
 			graph: [],
+			graphElementsMax: 1,
 			page: 1,
 
 			tickerCurrent: null,
@@ -30,8 +31,16 @@ export default {
 
 		if (tickersData) {
 			this.tickers = JSON.parse(tickersData);
-			this.tickers.forEach(ticker => subscribeToTicker(ticker.name, (price) => this.tickerUpdate(ticker.name, price)));
+			this.tickers.forEach(ticker => subscribeToTicker(ticker.name, price => this.tickerUpdate(ticker.name, price)));
 		}
+	},
+
+	mounted() {
+		window.addEventListener('resize', this.graphElementsMaxCalculate);
+	},
+
+	beforeUnmount() {
+		window.removeEventListener('resize', this.graphElementsMaxCalculate);
 	},
 
 	computed: {
@@ -75,6 +84,20 @@ export default {
 	},
 
 	methods: {
+		graphElementsMaxCalculate() {
+			if (!this.$refs.graph) {
+				return;
+			}
+
+			this.graphElementsMax = this.$refs.graph.clientWidth / 40;
+		},
+
+		graphResize() {
+			if (this.graph.length > this.graphElementsMax) {
+				this.graph = this.graph.slice(this.graph.length - this.graphElementsMax);
+			}
+		},
+
 		priceFormat(price) {
 			if (price === '-') {
 				return price;
@@ -98,13 +121,13 @@ export default {
 			this.tickerToAddName = '';
 			this.filter = '';
 
-			subscribeToTicker(tickerToAdd.name, (price) => this.tickerUpdate(tickerToAdd.name, price));
+			subscribeToTicker(tickerToAdd.name, price => this.tickerUpdate(tickerToAdd.name, price));
 		},
 
 		tickerDelete(tickerName) {
 			this.tickers = this.tickers.filter(ticker => ticker.name !== tickerName);
 
-			if (this.tickerCurrent.name === tickerName) {
+			if (tickerName === this.tickerCurrent?.name) {
 				this.tickerCurrent = null;
 			}
 
@@ -112,16 +135,21 @@ export default {
 		},
 
 		tickerSelect(ticker) {
+			if (ticker.name === this.tickerCurrent?.name) {
+				return;
+			}
+
 			this.tickerCurrent = ticker;
 		},
 
 		tickerUpdate(tickerName, price) {
-			this.tickers.filter(ticker => ticker.name === tickerName)
+			this.tickers.filter(tf => tf.name === tickerName)
 				.forEach(ticker => {
 					ticker.price = price;
 
-					if (ticker.name === this.tickerCurrent.name) {
+					if (ticker.name === this.tickerCurrent?.name) {
 						this.graph.push(price);
+						this.graphResize();
 					}
 				});
 		}
@@ -130,6 +158,10 @@ export default {
 	watch: {
 		filter() {
 			this.page = 1;
+		},
+
+		graphElementsMax() {
+			this.graphResize();
 		},
 
 		pageState(value) {
@@ -200,7 +232,7 @@ export default {
 			</button>
 		</section>
 
-		<template v-if='tickers.length'>
+		<template v-if="tickers.length">
 			<hr class="w-full border-t border-gray-600 my-4" />
 
 			<!-- Filter -->
@@ -221,9 +253,7 @@ export default {
 			<dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
 				<div v-for='ticker in tickersPaginated' :key='ticker.name'
 					@click='tickerSelect(ticker)'
-					:class='{
-						"border-4": tickerCurrent === ticker
-					}'
+					:class='{ "border-4": tickerCurrent === ticker }'
 					class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer">
 					<div class="px-4 py-5 sm:p-6 text-center">
 						<dt class="text-sm font-medium text-gray-500 truncate">
@@ -252,14 +282,14 @@ export default {
 				<h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
 					{{ tickerCurrent.name }} - USD
 				</h3>
-				<div class="flex items-end border-gray-600 border-b border-l h-64">
+				<div ref="graph" class="flex items-end border-gray-600 border-b border-l h-64">
 					<div v-for='(bar, idx) in graphNormalized' :key='idx'
 						:style='{ height: `${bar}%` }'
 						class="bg-purple-800 border w-10"
 					>
 					</div>
 				</div>
-				<button @click='tickerCurrent = null' type="button" class="absolute top-0 right-0">
+				<button @click="tickerCurrent = null" type="button" class="absolute top-0 right-0">
 					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs"
 						version="1.1" width="30" height="30" x="0" y="0" viewBox="0 0 511.76 511.76" style="enable-background:new 0 0 512 512" xml:space="preserve">
 						<g><path d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z" fill="#718096" data-original="#000000"></path></g>
